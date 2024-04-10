edb9b6f9a901fd6d6b2e393027250ecef5b06baa
Mateusz Chudkowski <120587768+chudkowsky@users.noreply.github.com>
2024-04-10 08:40:35 +0200

    Diffrent approach to zadanie2.rs

--- Z01/zadanie2.rs
+++ Z01/zadanie2.rs
@@ -1,31 +1,30 @@
 //Napisz program, który wyświetla liczbę dni miesiąca na podstawie jego numeru i numeru roku.
-fn if_leap_year(year1:i32)->bool{
-    if year1 % 4 == 0 &&( ( year1 % 100 == 0 && year1 % 400 == 0) || year1 % 100 != 0 ){
-            return true;
-        }else{
-            return false;
+fn leap_year(year: usize)->bool{
+    (year%4==0 && year%100!=0) || year%400 == 0
+    
+}
+
+fn days_in_month(year: usize, month: usize)->usize{
+   match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        2 => {
+            if leap_year(year) {
+                29
+            } else {
+                28
             }
-        }
-
-
+        }, 
+        _ => 0, // Invalid month number
+    }
+}
 fn main() {
     let years = [2000,1900];
     let months = [1,2,3,4,5,6,7,8,9,10,11,12];
     for year in years{
         for month in months{
-            if (month == 1 ||month == 3 || month ==5 || month ==7 || month ==8 || month ==10 || month ==12){
-                println!("{} : Miesiac ma 31 dni",month);}
-            else if month != 2 {
-                println!("{} : Miesiac ma 30 dni",month );}
-            else {
-                if if_leap_year(year){
-                    println!("{} : Miesiac ma 29 dni",month);
-                } else {
-                    println!("{} : Miesiac ma 28 dni",month);
-                }
-            }
+                    println!("{}",days_in_month(year,month));
         }
-
     }
 }
 
